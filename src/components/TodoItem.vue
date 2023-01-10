b679d3eea791328bf1b680a9c9a0b37809f99b8e
notDRKY <darkylyon@gmail.com>
2023-01-10 03:54:55 +0100

    rog init

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <li>
+    <li @dblclick="delTask">
       <!--
         La directiva v-model es un atajo que combina la directiva v-bind 
         para enlazar el valor del elemento con una propiedad, y la directiva 
         v-on para escuchar los eventos de cambio del elemento y actualizar 
         la propiedad.
       -->
-      <input v-model="taskInfo.completed" @dblclick="delTask" type="checkbox">
+      <input v-model="taskInfo.completed" type="checkbox">
       <del v-if="taskInfo.completed">{{ taskInfo.task }}</del>
       <span v-else>{{ taskInfo.task }}</span>
     </li>
